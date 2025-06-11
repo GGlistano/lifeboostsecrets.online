6b76b613b39c738e4256baaa850f827f982d2fa7
Agostinho Facitela <agostinhofacitela2006@gmail.com>
2025-06-11 18:17:22 +0200

    Update data.js

--- mov2/js/data.js
+++ mov2/js/data.js
@@ -1,37 +1,37 @@
 const quizData = [
   {
     question: 'Em que ano a Movitel foi fundada?',
-    image: 'img/2mov.jpg',
+    image: 'imagens/IMAGEM 2.jpg',
     options: ['2005', '2008', '2011', '2015']
   },
   {
     question: 'Quantas províncias de Moçambique são atendidas pelos serviços da Movitel?',
-    image: 'img/8mov.png',
+    image: 'imagens/IMAGEM 5.png',
     options: ['Apenas 5 províncias', '8 províncias', 'Todas as 11 províncias', 'Apenas 2 províncias']
   },
   {
     question: 'Qual é o slogan atual da Movitel?',
-    image: 'img/11mov.jpg',
+    image: 'imagens/IMAGEM 6.jpg',
     options: ['Sempre em Movimento', 'A rede que te conecta', 'Ligando Moçambique', 'Em todo lugar a todo momento']
   },
   {
     question: 'Quais destes serviços são oferecidos pela Movitel?',
-    image: 'img/10mov.jpg',
+    image: 'imagens/IMAGEM 7.jpg',
     options: ['m-Pesa', 'm-Kesh', 'emola', 'soprati']
   },
   {
     question: 'Quem são os principais concorrentes da Movitel no mercado moçambicano?',
-    image: 'img/4mov.jpg',
+    image: 'imagens/IMAGEM 8.jpg',
     options: ['Tmcel e Vodacom', 'Orange e Airtel', 'Mcel e Unitel', 'Tim e Unitel']
   },
   {
     question: 'Que tipo de tecnologia a Movitel oferece em seus serviços?',
-    image: 'img/3mov.jpg',
+    image: 'imagens/IMAGEM 9.jpg',
     options: ['Apenas 3G', '4G e fibra óptica', '2G e rede de satélite', 'Wi-Fi gratuito']
   },
   {
     question: 'Você recomendaria a sua família a usar a Movitel?',
-    image: 'img/5mov.jpg',
+    image: 'imagens/IMAGEM 10.jpg',
     options: ['Sim', 'Não']
   }
 ];
